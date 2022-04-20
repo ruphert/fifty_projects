444d3096606d34cd3db15c65f1f27dcc8f71cbbe
ruphert <ruphert6@gmail.com>
2022-04-20 18:00:55 +0800

    finished the functionality, bugfix in html

---
+++ key_codes/scripts.js
@@ -0,0 +1,21 @@
+const insert = document.getElementById("insert");
+
+window.addEventListener("keydown", (event) => {
+  insert.innerHTML = `
+  <div class="key">
+        ${event.key === " " ? "Space" : event.key}
+        <small>event.key</small>
+      </div>
+
+ 
+
+      <div class="key">
+        ${event.code}
+        <small>event.code</small>
+      </div>`;
+});
+// DEPRECATED
+//  <div class="key">
+//     ${event.keycode}
+//     <small>event.keyCode</small>
+//   </div>
