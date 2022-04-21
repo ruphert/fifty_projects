dd44f16f8365258014f409d7f712c1a7fda43d7d
ruphert <ruphert6@gmail.com>
2022-04-21 16:44:33 +0800

    added extra css, added functionality to js

---
+++ FAQ/scripts.js
@@ -0,0 +1,8 @@
+const faqs = document.querySelectorAll(".faq");
+const toggles = document.querySelectorAll(".faq-toggle");
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    toggle.parentNode.classList.toggle("active");
+  });
+});
